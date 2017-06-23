d15b20f140a59a77e09e11f81c9f64f956c7d8f5
jorge <jorge.f@velosoft.mx>
2017-06-23 13:13:22 -0500

    Proveedores 2 3 4 5

---
+++ proveedores-02.php
@@ -0,0 +1,265 @@
+<?php
+  include("head-3.php");
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Main content -->
+  <section class="content">
+    <div class="row">
+      <!-- /.col -->
+      <div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3 titulo-usuario51">
+        <div class="col-md-6 col-md-offset-3 titulo-perfil">
+          <p class="rotateInUpLeft ">
+            proveedores
+          </p>
+        </div>
+        <img src="images/recursos/linea-greca.png" alt="The Wedding Board" width="100%">
+      </div>
+      <div class="col-md-6 col-md-offset-3 col-xs-12 top-20 botton-volver">
+        <div class="col-xs-3 col-sm-3 col-md-4 prev-next nomargin">
+          <img src="images/dashboard/anterior.png" alt="The Wedding Board">
+          <span>Anterior</span>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 centers nomargin">
+          <a href="">
+            <p class="usuario5-volver" style="margin: 0 auto;">
+              <img src="images/dashboard/volver.png" alt="The Wedding Board"> Volver a Listado
+            </p>
+          </a>
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-4 prev-next1 nomargin">
+          <span>Siguiente</span>
+          <img src="images/dashboard/siguiente.png" alt="The Wedding Board">
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 linea-config"></div>
+
+      <div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 margin-men">
+        <div class="col-xs-12 col-sm-6 col-md-3 centers margin-volver">
+            <a href="">
+              <p class="usuario5 margin-0">
+                <i class="fa fa-envelope-o" aria-hidden="true"></i> Enviar Mensajes
+              </p>
+            </a>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 centers margin-volver">
+            <a href="">
+              <p class="usuario5 margin-0">
+                <i class="fa fa-ban" aria-hidden="true"></i> Inhabilitar
+              </p>
+            </a>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 centers margin-volver">
+            <a href="">
+              <p class="usuario5 margin-0 pro">
+                <i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar
+              </p>
+            </a>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 centers margin-volver">
+            <a href="">
+              <p class="usuario5 margin-0 pro">
+                <i class="fa fa-pencil" aria-hidden="true"></i> Editar
+              </p>
+            </a>
+          </div>
+        </div>
+
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 linea-config"></div>
+        <div class="col-xs-12 col-md-12 nomargin img-twb1 centers">
+          <img src="images/proveedores/1.png" class="img-circle" alt="The Wedding Board">
+          <div class="col-xs-12 col-md-12 col-lg-12 text-nombre">
+            <p style="margin:5px 0 0 0;">JL Event Planner</p>
+            <span> Decoration</span>
+          </div>
+        </div>
+        <div class="form-group row top-30 centrado">
+          <div class="col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2">
+            <div class="star-rating">
+              <fieldset>
+                <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Outstanding">5 stars</label>
+                <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Very Good">4 stars</label>
+                <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Good">3 stars</label>
+                <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Poor">2 stars</label>
+                <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Very Poor">1 star</label>
+              </fieldset>
+            </div>
+          </div>
+        </div>
+        <div class="form-group row top-30 centrado">
+          <div class="col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2">
+            <a href="#!"><p type="submit" class="btn boton-proveedores">Ver Calificaciones</p></a>
+          </div>
+        </div>
+
+      <!-- /.col -->
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 linea-config"></div>
+
+        <div class="col-sm-11 col-sm-offset-1 col-md-12 col-lg-10 col-lg-offset-1">
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 centrado">
+            <p class="text-detalle1">DETALLES</p>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span">
+            <div class="col-sm-6 col-md-6 col-lg-6">
+              <p>Nombre Proveedor: <span> JL Event Planner</span></p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-l6">
+                <p>Mail: <span>jl.event.planner@mailtest.mx</span></p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span">
+            <div class=" col-sm-6 col-md-6">
+              <p>Categoria: <span>Recepción</span></p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-l6">
+                <p>Teléfono: <span>55 20 86 54 74</span></p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span">
+            <div class=" col-sm-6 col-md-6">
+              <p>Sub categoria: <span>Haciendas</span></p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-l6">
+                <p>Lacalidad: <span>Ciudad de México</span></p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span">
+            <div class=" col-sm-6 col-md-6">
+              <p>Membresía: <span>Tipo A</span></p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-l6">
+                <p>Miembro desde: <span>25 Abril 2017</span></p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span bottom-evento">
+            <div class="col-sm-6 col-md-6">
+              <p>Vencimiento: <span>25 Abril 2017</span></p>
+            </div>
+            <div class="col-sm-6 col-md-6">
+                <p>Aprobado por: <span>Shopie Dueso</span></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group row top-50 centrado">
+          <div class="col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2">
+            <a href="#!"><p type="submit" class="btn boton-calificaciones">Calificaciones a Proveedores</p></a>
+          </div>
+        </div>
+
+        <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 linea-config"></div>
+
+        <div class="col-sm-11 col-sm-offset-1 col-md-12 col-lg-10 col-lg-offset-1 padin-cero-conf">
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 centrado">
+            <p class="text-detalle1">ESTADÍSTICAS</p>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span">
+            <div class="col-sm-6 col-md-6 col-lg-6">
+              <p>Tiempo de respuesta: <span>1 Hora</span></p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <p>Mensajes recibidos: <span>25</span></p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span">
+            <div class=" col-sm-6 col-md-6">
+              <p>Número de visitas: <span>25</span></p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <p>Licitaciones: <span>10</span></p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span">
+            <div class="col-sm-6 col-md-6">
+              <p>Cotizaciones envíadas: <span>30</span></p>
+            </div>
+            <div class="col-sm-6 col-md-6">
+                <p>Licitaciones ganadas: <span>3</span></p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-span bottom-evento">
+            <div class="col-sm-6 col-md-6">
+              <p>Mensajes envíados: <span>5</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 linea-config"></div>
+
+        <div class="col-sm-11 col-sm-offset-1 col-md-12 col-lg-10 col-lg-offset-1 padin-cero-conf">
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 centrado move-red">
+            <p class="text-detalle1">RED DE COLABORACION</p>
+          </div>
+          <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+            <div class="col-sm-12 col-md-12 col-lg-12 centrado red">
+              <p>AÚN NO TIENE COLABORADORES <br> EN SU RED</p>
+            </div>
+          </div>
+          <div class="form-group row top-50 centrado">
+          <div class="col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2 botton-red">
+            <a href="#!"><p type="submit" class="btn boton-proveedores">Ver Toda la Red</p></a>
+          </div>
+        </div>
+        </div>
+
+
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 linea-config"></div>
+      <div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 margin-men">
+        <div class="col-xs-12 col-sm-6 col-md-3 centers margin-volver">
+            <a href="">
+              <p class="usuario5 margin-0">
+                <i class="fa fa-envelope-o" aria-hidden="true"></i> Enviar Mensajes
+              </p>
+            </a>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 centers margin-volver">
+            <a href="">
+              <p class="usuario5 margin-0">
+                <i class="fa fa-ban" aria-hidden="true"></i> Inhabilitar
+              </p>
+            </a>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 centers margin-volver">
+            <a href="">
+              <p class="usuario5 margin-0 pro">
+                <i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar
+              </p>
+            </a>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 centers margin-volver">
+            <a href="">
+              <p class="usuario5 margin-0 pro">
+                <i class="fa fa-pencil" aria-hidden="true"></i> Editar
+              </p>
+            </a>
+          </div>
+        </div>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 linea-config"></div>
+      <div class="col-md-6 col-md-offset-3 col-xs-12 top-30 botton-volver">
+        <div class="col-xs-3 col-sm-3 col-md-4 prev-next nomargin">
+          <img src="images/dashboard/anterior.png" alt="The Wedding Board">
+          <span>Anterior</span>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 centers nomargin">
+          <a href="">
+            <p class="usuario5-volver" style="margin: 0 auto;">
+              <img src="images/dashboard/volver.png" alt="The Wedding Board"> Volver a Listado
+            </p>
+          </a>
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-4 prev-next1 nomargin">
+          <span>Siguiente</span>
+          <img src="images/dashboard/siguiente.png" alt="The Wedding Board">
+        </div>
+      </div>
+
+    </div>
+    <!-- /.row -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<div class="control-sidebar-bg"></div>
+</div>
+<?php
+	include("footer-3.php");
+?>
