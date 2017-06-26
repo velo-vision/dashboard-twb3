6dc3d999e074622d51abf2991ab33366b7b84b76
esteban20 <esteban@velosoft.mx>
2017-06-26 13:43:09 -0500

    template 2 terminado

--- template-1.php
+++ template-1.php
@@ -30,7 +30,7 @@
           <div class="box-body no-padding">
             <div class="mailbox-controls"> 
              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 centro aumento-temp">
-              <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 centro">
+              <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 centro separacion-template-selects">
               <form>
                 <select class="select-template">
                   <option value="#">Categoria</option>
@@ -40,7 +40,7 @@
                 </select>
                 </form>
               </div>
-              <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 centro">
+              <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 centro separacion-template-selects">
               <form>
                 <select class="select-template">
                   <option value="#">Precio</option>
@@ -50,7 +50,7 @@
                 </select>
                 </form>
               </div>
-              <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 centro">
+              <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 centro separacion-template-selects">
               <form>
                 <select class="select-template">
                   <option value="#">Tipo</option>
@@ -74,12 +74,12 @@
               <a href="#!"><img width="100%" src="images/invitaciones/3.jpg" alt="The Wedding Board"></a>
 
                <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -93,12 +93,12 @@
               </div>
               <a href=""><img width="100%" src="images/invitaciones/11.jpg" alt="The Wedding Board"></a>
               <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -111,12 +111,12 @@
               </div>
               <a href=""><img width="100%" src="images/invitaciones/7.png" alt="The Wedding Board"></a>
               <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -131,12 +131,12 @@
               </div>
               <a href=""><img width="100%" src="images/invitaciones/8.jpg" alt="The Wedding Board"></a>
               <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -149,12 +149,12 @@
               </div>
               <a href=""><img width="100%" src="images/invitaciones/9.jpg" alt="The Wedding Board"></a>
               <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -167,12 +167,12 @@
               </div>
               <a href=""><img width="100%" src="images/invitaciones/10.jpg" alt="The Wedding Board"></a>
               <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -187,12 +187,12 @@
               </div>
               <a href=""><img width="100%" src="images/invitaciones/1.jpg" alt="The Wedding Board"></a>
               <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -205,12 +205,12 @@
               </div>
               <a href=""><img width="100%" src="images/invitaciones/2.jpg" alt="The Wedding Board"></a>
               <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -223,12 +223,12 @@
               </div>
               <a href=""><img width="100%" src="images/invitaciones/5.jpg" alt="The Wedding Board"></a>
               <div class="col-xs-12 co-sm-12 col-md-12 col-lg-12 centro title-temp margin15">
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro aumento-template1">
                   <a data-toggle="modal" data-target="#ocultar" data-whatever="@mdo">
-                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-eye-slash" aria-hidden="true"></i> Ocultar</button>
+                    <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Publicar</button>
                   </a>
                 </div>
-                <div class="col-xs-6 co-sm-6 col-md-6 col-lg-6 centro">
+                <div class="col-xs-6 co-sm-6 col-md-12 col-lg-6 centro">
                   <a data-toggle="modal" data-target="#delete" data-whatever="@mdo">
                     <button type="submit" class="btn btn-default icon-menssage"><i class="fa fa-trash-o" aria-hidden="true"></i> Eliminar</button>
                   </a>
@@ -254,17 +254,19 @@
             <div class="modal-body col-md-12" style="background:white;">
               <!--COLLAPSER-->
                 <div class="col-md-12">
-                  <p class="titulo-popoup" style="text-transform:capitalize">
-                   Ocultar<span> In</span>vitación
+                  <p class="titulo-popoup">
+                   Publica<span>r In</span>vitación
                   </p> 
-                  <P class="titulo-popoup">
-                    Esta invitación dejará de ser visible en TWB<br>¿Esta seguro que desea continuar?
+                  <P class="titulo-popoup normal-text">
+                    Esta invitación sera visible en TWB<br>¿Esta seguro que desea continuar?
                   </P>                 
                 </div>
 
                 <div class="col-md-8 col-md-offset-2 top-20">
                   <div class="col-md-6" style="text-align:right">
+                  <a href="template-2.php">
                     <button type="submit" class="btn btn-default icon-menssage"><!--<i class="fa fa-check-square-o" aria-hidden="true"></i> -->Continuar</button>
+                  </a>
                   </div>
                   <div class="col-md-6">
                     <button type="submit" class="btn btn-default icon-menssage" data-dismiss="modal" aria-label="Close">
@@ -280,7 +282,7 @@
       <!--MODAL-->
 
       <!--MODAL 2-->
-      <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <!-- <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog content-popoup" role="document">
           <div class="modal-content">
             <div class="modal-header">
@@ -288,9 +290,9 @@
               <span aria-hidden="true"><img src="images/recursos/close-popoup.png" alt="The Wedding Board"> cerrar</span>
               </button>
             </div>
-            <div class="modal-body col-md-12" style="background:white;">
+            <div class="modal-body col-md-12" style="background:white;"> -->
               <!--COLLAPSER-->
-                <div class="col-md-12">
+                <!-- <div class="col-md-12">
                   <p class="titulo-popoup" style="text-transform:capitalize">
                     VAS AÑADIR  COMO PROVEEDOR A:
                   </p>
@@ -307,12 +309,12 @@
                     <button type="submit" class="btn btn-default icon-menssage" data-dismiss="modal" aria-label="Close"><i class="fa fa-times" aria-hidden="true"></i> Cancelar</button>
                   </div>
 
-                </div>
+                </div> -->
               <!--COLLAPSER-->
-            </div>
+            <!-- </div>
           </div>
         </div>
-      </div>
+      </div> -->
       <!--MODAL 2-->
 
 
