0ec9b91c04fb0266a4c96369c37e09d753da7703
esteban20 <esteban@velosoft.mx>
2017-07-10 10:17:47 -0500

    te pasas

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
              </div>
              <div class="form-group row top-20">
                <div class="col-md-8 col-md-offset-2">
-                 <a href="dashboard.php"><button type="button" class="btn boton-registro ">INGRESAR</button></a>
+                 <a href="reportes.php"><button type="button" class="btn boton-registro ">INGRESAR</button></a>
                </div>
              </div>
              <div class="form-group row top-10">
